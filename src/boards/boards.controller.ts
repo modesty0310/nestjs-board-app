ad3a99880997f0abfdf1702e4d0798a5bd0c9fb1
modesty0310 <rlatprua4@gmail.com>
2022-01-14 11:36:33 +0900

    create board controller

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
+  @Get('/')
   getAllBoard() {
     return this.boardsService.getAllBoards();
+  }
+
+  // @Post('/create')
+  // createBoard(@Req() request: Request) {
+  //   this.boardsService.createBoards(
+  //     request.body.title,
+  //     request.body.description,
+  //   );
+  // }
+
+  @Post('/create')
+  createBoard(@Body() board: Board) {
+    this.boardsService.createBoards(board.title, board.description);
   }
 }
