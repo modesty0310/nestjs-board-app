ec2950ad6ba43f40896bed72ff32500c5870f932
modesty0310 <rlatprua4@gmail.com>
2022-01-19 20:19:29 +0900

    typeorm config 생성

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -7,7 +7,7 @@ export class BoardStatusValidation implements PipeTransform {
   transform(value: any) {
     value = value.toUpperCase();
     if (!this.isStatusValid(value)) {
-      throw new BadRequestException('잘못된 스테이 터스입니다.');
+      throw new BadRequestException('잘못된 스테이터스입니다.');
     }
   }
 
